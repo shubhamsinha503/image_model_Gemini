7900e6a246fbcc2216145efcf6029581d7d09986
MANOJ AJJAKANA SHYAM <144515384+Manojajj@users.noreply.github.com>
2024-08-24 16:10:26 +0530

    Update image_model.py

--- image_model.py
+++ image_model.py
@@ -8,6 +8,15 @@ import google.generativeai as genai
 # Initialize the Streamlit app
 st.set_page_config(page_title="Gemini Vision Bot Demo")
 
+# Function to load Gemini AI model and get the reponse
+def get_gemini_response(input_text, image):
+    model = genai.GenerativeModel("gemini_pro_vision")
+    if input_text != "":
+        response = model.generate_content([input_text, image])
+    else:
+        response = model.generate_content(image)
+    return response.text
+
 st.header("Gemini Application")
 
 # Input field for the Google API Key
@@ -35,12 +44,3 @@ if submit and api_key and image is not None:
     response = get_gemini_response(input_text, image)
     st.subheader("The Response is:")
     st.write(response)
-
-# Function to load Gemini AI model and get the reponse
-def get_gemini_response(input_text, image):
-    model = genai.GenerativeModel("gemini_pro_vision")
-    if input_text != "":
-        response = model.generate_content([input_text, image])
-    else:
-        response = model.generate_content(image)
-    return response.text
