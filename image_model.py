808229a113b605e42e2d34f3ee48956ca06d407e
MANOJ AJJAKANA SHYAM <144515384+Manojajj@users.noreply.github.com>
2024-08-24 16:11:31 +0530

    Update image_model.py

--- image_model.py
+++ image_model.py
@@ -5,22 +5,25 @@ import os
 from PIL import Image
 import google.generativeai as genai
 
-# Initialize the Streamlit app
-st.set_page_config(page_title="Gemini Vision Bot Demo")
+# Load environment variables from .env file (Optional)
+load_dotenv()
 
-# Function to load Gemini AI model and get the reponse
+# Function to load Gemini AI model and get the response
 def get_gemini_response(input_text, image):
-    model = genai.GenerativeModel("gemini_pro_vision")
+    model = genai.GenerativeModel("gemini-pro-vision")
     if input_text != "":
         response = model.generate_content([input_text, image])
     else:
         response = model.generate_content(image)
     return response.text
 
+# Initialize the Streamlit app
+st.set_page_config(page_title="Gemini Vision Bot Demo")
+
 st.header("Gemini Application")
 
 # Input field for the Google API Key
-api_key = st.text_input("Enter Your Gemini API Key: ", type="password")
+api_key = st.text_input("Enter your Gemini API Key: ", type="password")
 
 # Input prompt from the user
 input_text = st.text_input("Input Prompt: ", key="input")
@@ -36,11 +39,11 @@ if uploaded_file is not None:
 # Button to submit the request
 submit = st.button("Tell me about the image")
 
-# if the streamlit button is clicked, configure the API key and get the Gemini AI response
+# If the submit button is clicked, configure the API key and get the Gemini AI response
 if submit and api_key and image is not None:
     os.environ["GOOGLE_API_KEY"] = api_key
     genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
-
+    
     response = get_gemini_response(input_text, image)
     st.subheader("The Response is:")
     st.write(response)
